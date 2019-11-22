5acf7fc666efcd2e9d773512cff7cfcc4a6441d3
bailcait <42756064+bailcait@users.noreply.github.com>
2019-11-21 22:40:54 -0700

    Main file

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+import javax.naming.directory.InvalidSearchControlsException;
+
+public class Main {
+	
+
+	
+	
+	public static void main(String[] args) {
+		Parser p = new Parser();
+		p.FillShelves();
+		
+		ArrayList<Node> nodes = new ArrayList<Node>();
+		
+		while(p.ids.size() >1) {
+			String name = p.ids.get(0);
+			p.ids.remove(0);
+			nodes.add(new Node(name, p.ids, p.shelves));
+			
+		}
+
+		
+		System.out.print("Test");
+	}
+	
+
+}
