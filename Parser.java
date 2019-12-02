f8097690e57f6383d0f095d277cffe8e79855146
bailcait <42756064+bailcait@users.noreply.github.com>
2019-12-02 15:08:14 -0700

    updated to use books file

--- Parser.java
+++ Parser.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Parser {
 
@@ -11,23 +12,27 @@ public class Parser {
 	//help for file reading and parsing found at https://www.reddit.com/r/javaexamples/comments/344kch/reading_and_parsing_data_from_a_file/
 	public void FillShelves() {
 		try {
-			FileReader input = new FileReader("./data/data.txt");
+			FileReader input = new FileReader("./data/books.txt");
 			BufferedReader br = new BufferedReader(input);
 			String fileRead = br.readLine();
 			boolean cont = true;
 			while(cont) {
 				String[] booksFromLine = fileRead.split(" ");
-				shelves.add(booksFromLine);
-				//First index will be the user id
-				for (int i = 0; i < booksFromLine.length; i++) {
-					if (ids.contains(booksFromLine[i]) == false) {
-						ids.add(booksFromLine[i]);
+				if(booksFromLine.length > 11) {
+					booksFromLine = Arrays.copyOfRange(booksFromLine, 0, 11);
+				}	
+					shelves.add(booksFromLine);
+					//First index will be the user id
+					for (int i = 1; i < booksFromLine.length; i++) {
+						if (ids.contains(booksFromLine[i]) == false) {
+							ids.add(booksFromLine[i]);
+						}
 					}
-				}
-				fileRead = br.readLine();
-				if(fileRead == null) {
-					cont = false;
-				}
+					fileRead = br.readLine();
+					if(fileRead == null) {
+						cont = false;
+					}
+				
 			}
 			System.out.println("Sucessfully read file");
 		}
