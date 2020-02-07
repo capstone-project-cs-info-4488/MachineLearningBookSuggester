db0f76a487698e50569fb9ab9a603ca9946bebd9
Rosetta Roberts <roberose@isu.edu>
2020-02-07 16:48:46 -0700

    added android project (in kotlin)

---
+++ java/android/src/main/java/edu/isu/capstone/bookrec/android/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.isu.capstone.bookrec.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
