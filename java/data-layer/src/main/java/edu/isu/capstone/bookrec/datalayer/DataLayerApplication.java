9a086d1aa00cbb7ea7bbe99b68a2fee98e90a108
Brandon Uehlein <uehlbran@isu.edu>
2020-02-06 17:52:27 -0700

    Added Spring Boot to project.

---
+++ java/data-layer/src/main/java/edu/isu/capstone/bookrec/datalayer/DataLayerApplication.java
@@ -0,0 +1,13 @@
+package edu.isu.capstone.bookrec.datalayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataLayerApplication.class, args);
+    }
+
+}
