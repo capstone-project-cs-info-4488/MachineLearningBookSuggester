0714fa4630e651c92cee05ac2f0b7d0423962e26
Brandon Uehlein <uehlbran@isu.edu>
2020-02-16 20:38:02 -0700

    Removed duplicated method, configure, in AuthenticationSuccessHandler.

--- java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/configuration/WebSecurityConfig.java
+++ java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/configuration/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package edu.isu.capstone.bookrec.backend.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -27,11 +26,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().sessionManagement()
                     .maximumSessions(1)
                     .maxSessionsPreventsLogin(true);
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        throw new NotImplementedException();
     }
 
     @Bean
--- java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/repositories/UserRepository.java
+++ java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package edu.isu.capstone.bookrec.backend.repositories;
 
 import edu.isu.capstone.bookrec.backend.hibernate.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
 @Transactional
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
