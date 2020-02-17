8fbdbee6a2ccf11774b0db2e058d5c1c36514d25
Rosetta Roberts <rosettafroberts@gmail.com>
2020-02-16 20:44:46 -0700

    Added task to build everything

--- java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/configuration/WebSecurityConfig.java
+++ java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/configuration/WebSecurityConfig.java
@@ -1,13 +1,9 @@
 package edu.isu.capstone.bookrec.backend.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 @Configuration
 @EnableWebSecurity
@@ -17,25 +13,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic()
                 .and().authorizeRequests()
                 .antMatchers("/resources/**", "/css/**", "/js/**")
-                    .permitAll()
+                .permitAll()
                 .antMatchers("/**")
-                    .permitAll()
+                .permitAll()
                 .anyRequest()
-                    .authenticated()
+                .authenticated()
                 .and().csrf()
-                    .disable().headers().frameOptions().disable()
+                .disable().headers().frameOptions().disable()
                 .and().sessionManagement()
-                    .maximumSessions(1)
-                    .maxSessionsPreventsLogin(true);
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        throw new NotImplementedException();
-    }
-
-    @Bean
-    public AuthenticationSuccessHandler authenticationSuccessHandler() {
-        throw new NotImplementedException();
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true);
     }
 }
