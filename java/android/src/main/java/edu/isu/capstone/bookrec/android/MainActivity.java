3f6fb7b2f6b13143496cdb592d46f810d70c7032
Rosetta Roberts <rosettafroberts@gmail.com>
2020-03-02 00:55:53 -0700

    got linter to let things compile

--- java/android/src/main/java/edu/isu/capstone/bookrec/android/MainActivity.java
+++ java/android/src/main/java/edu/isu/capstone/bookrec/android/MainActivity.java
@@ -1,16 +1,15 @@
 package edu.isu.capstone.bookrec.android;
 
 import android.content.Intent;
-import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.app.Activity;
 import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.TextView;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
 import edu.isu.capstone.bookrec.android.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,13 +17,15 @@ public class MainActivity extends AppCompatActivity {
 
     TextView Hyperlink;
     Spanned Text;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Hyperlink = (TextView)findViewById(R.id.txtRegister);
-        Text = Html.fromHtml("<a href='https://goodreads.com/register'>Click here to register.");
+        Hyperlink = findViewById(R.id.txtRegister);
+        Text = Html.fromHtml("<a href='https://goodreads.com/register'>Click here to register.", Html.FROM_HTML_MODE_LEGACY);
 
         Hyperlink.setMovementMethod(LinkMovementMethod.getInstance());
         Hyperlink.setText(Text);
