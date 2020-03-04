a13c21068e7d428e96e4c1a05f7144b87804af6e
Brandon Uehlein <uehlbran@isu.edu>
2020-03-04 08:00:58 -0700

    Added controllers for User class and Main controller for testing.

---
+++ java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/controllers/MainController.java
@@ -0,0 +1,12 @@
+package edu.isu.capstone.bookrec.backend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @RequestMapping("")
+    public String testController() {
+        return "Response was successful";
+    }
+}
---
+++ java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/controllers/UserController.java
@@ -0,0 +1,12 @@
+package edu.isu.capstone.bookrec.backend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @RequestMapping("/login")
+    public void Login() {
+
+    }
+}
