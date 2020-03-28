8c52f31ba613327700095bee35a83efd6b708a49
Caitlyn Bailey <bailcait@isu.edu>
2020-03-28 16:55:21 -0600

    added click events to each image in the recomendations and library to direct to the book page

--- java/android/src/main/java/edu/isu/capstone/bookrec/android/ui/home/HomeFragment.java
+++ java/android/src/main/java/edu/isu/capstone/bookrec/android/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package edu.isu.capstone.bookrec.android.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,12 @@ import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.squareup.picasso.Picasso;
+
+import edu.isu.capstone.bookrec.android.BookView;
+import edu.isu.capstone.bookrec.android.MainActivity;
 import edu.isu.capstone.bookrec.android.R;
+import edu.isu.capstone.bookrec.android.ui.login.LoginActivity;
+
 import java.io.*;
 
 public class HomeFragment extends Fragment {
@@ -37,7 +43,6 @@ public class HomeFragment extends Fragment {
         for(int i = 0; i < numBooksLib; i++) {
             //create new img then set parameters
             ImageView img = new ImageView(getContext());
-
             LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(300, 400);
             lp.setMargins(15, 0, 15, 0);
             img.setLayoutParams(lp);
@@ -46,6 +51,16 @@ public class HomeFragment extends Fragment {
             //url to load into image
             //How to change url for each unique book? Load array with urls? Where do you get the urls?
             Picasso.get().load("https://i.imgur.com/aEggkZr.jpg").placeholder(R.mipmap.ic_launcher).into(img);
+            //For adding click events to each image
+            //https://stackoverflow.com/questions/1839273/how-to-apply-click-event-listener-to-image-in-android/1839454
+            img.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //https://stackoverflow.com/questions/15478105/start-an-activity-from-a-fragment
+                    Intent intent = new Intent(getActivity(),BookView.class);
+                    startActivity(intent);
+                }
+            });
             //add img to linearlayout
             lib.addView(img);
         }
@@ -67,6 +82,16 @@ public class HomeFragment extends Fragment {
             //url to load into image
             //How to change url for each unique book? Load array with urls? Where do you get the urls?
             Picasso.get().load("https://i.imgur.com/aEggkZr.jpg").placeholder(R.mipmap.ic_launcher).into(img);
+            //For adding click events to each image
+            //https://stackoverflow.com/questions/1839273/how-to-apply-click-event-listener-to-image-in-android/1839454
+            img.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //https://stackoverflow.com/questions/15478105/start-an-activity-from-a-fragment
+                    Intent intent = new Intent(getActivity(),BookView.class);
+                    startActivity(intent);
+                }
+            });
             //add img to linearlayout
             rec.addView(img);
         }
