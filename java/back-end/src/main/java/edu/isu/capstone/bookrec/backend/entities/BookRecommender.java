be28a767945d2cb54b17843def8b7045c51ace20
Brandon Uehlein <uehlbran@isu.edu>
2020-03-29 20:35:05 -0600

    Turned BookRecommeder into a Spring managed component.

--- java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/entities/BookRecommender.java
+++ java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/entities/BookRecommender.java
@@ -1,9 +1,11 @@
 package edu.isu.capstone.bookrec.backend.entities;
 
+import org.springframework.stereotype.Component;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Set;
 
+@Component
 public class BookRecommender {
     public String recommendBook(Set<String> bookIds) {
         throw new NotImplementedException();
