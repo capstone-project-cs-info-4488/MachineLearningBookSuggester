f44e33e47141d9e40d5f2aa7b0fdc7af56a19b86
Brandon Uehlein <uehlbran@isu.edu>
2020-03-29 20:46:29 -0600

    Temporarily commented out all code related to BookRecommender in the backend code.

--- java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/configuration/BookRecommenderConfig.java
+++ java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/configuration/BookRecommenderConfig.java
@@ -1,26 +1,25 @@
-package edu.isu.capstone.bookrec.backend.configuration;
-import edu.isu.capstone.bookrec.backend.entities.BookRecommender;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
-
-;
-
-@Configuration
-public class BookRecommenderConfig {
-    private static final Logger logger = LoggerFactory.getLogger(BookRecommenderConfig.class);
-
-    @Bean
-    public BookRecommender getBookRecommender() throws IOException {
-        logger.info("Creating book recommender");
-        long start = System.currentTimeMillis();
-        BookRecommender recommender = BookRecommender.createDefault();
-        long elapsed = System.currentTimeMillis() - start;
-        logger.info("Created book recommender in {} ms", elapsed);
-
-        return recommender;
-    }
-}
+//package edu.isu.capstone.bookrec.backend.configuration;
+//
+//import edu.isu.capstone.bookrec.recommender.BookRecommender;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.io.IOException;
+//
+//@Configuration
+//public class BookRecommenderConfig {
+//    private static final Logger logger = LoggerFactory.getLogger(BookRecommenderConfig.class);
+//
+//    @Bean
+//    public BookRecommender getBookRecommender() throws IOException {
+//        logger.info("Creating book recommender");
+//        long start = System.currentTimeMillis();
+//        BookRecommender recommender = BookRecommender.createDefault();
+//        long elapsed = System.currentTimeMillis() - start;
+//        logger.info("Created book recommender in {} ms", elapsed);
+//
+//        return recommender;
+//    }
+//}
