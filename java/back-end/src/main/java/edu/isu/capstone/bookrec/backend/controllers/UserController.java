2b57bbbda8b14352637d8381e42182bbdcc805ab
Rosetta Roberts <rosettafroberts@gmail.com>
2020-04-30 15:06:12 -0600

    removed not implemented exception

--- java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/controllers/UserController.java
+++ java/back-end/src/main/java/edu/isu/capstone/bookrec/backend/controllers/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 @RestController
 public class UserController {
@@ -16,12 +15,12 @@ public class UserController {
 
     @GetMapping("/user/{id}/show")
     public String getUserById(@PathVariable Long id) {
-        throw  new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 
     @GetMapping("/user/{username}/show")
     public String getUserByUsername(@PathVariable String username) {
-        throw  new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 
     @GetMapping("/user/{userId}/bookshelf/{bookshelfId}")
@@ -37,28 +36,28 @@ public class UserController {
     public String addBook(@PathVariable Long userId,
                           @PathVariable Long bookshelfId,
                           @PathVariable Long bookId) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 
     @PostMapping("/user/{userId}/bookshelf/create")
     public String createBookshelf(@PathVariable Long userId) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 
     @DeleteMapping("/user/{userId}/bookshelf/{bookshelfId}/remove/{bookId}")
     public String removeBook(@PathVariable Long userId,
                              @PathVariable Long bookshelfId,
                              @PathVariable Long bookId) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 
     @GetMapping("/user/{userId}/bookshelves")
     public String getAllBookshelves(@PathVariable Long userId) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 
     @PutMapping("/user/{userId}/bookshelf/{bookshelfId}/update")
     public String updateBookshelf(@PathVariable Long userId, @PathVariable Long bookshelfId) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 }
