ced6c17e4883483eda9d53532e932a74d122940e
John Motley <motljohn@isu.edu>
2020-04-29 23:03:54 -0600

    Switched from gson to moshi because it was made by the same people as a lot of the technologies we're using.

--- java/android/src/main/java/edu/isu/capstone/bookrec/android/di/RetrofitModule.java
+++ java/android/src/main/java/edu/isu/capstone/bookrec/android/di/RetrofitModule.java
@@ -5,6 +5,7 @@ import dagger.Provides;
 import edu.isu.capstone.bookrec.android.data.datasources.remote.GoodreadsClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.moshi.MoshiConverterFactory;
 
 @Module
 class RetrofitModule {
@@ -12,7 +13,7 @@ class RetrofitModule {
     Retrofit retrofit() {
         return new Retrofit.Builder()
                 .baseUrl("https://www.goodreads.com/")
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(MoshiConverterFactory.create())
                 .build();
     }
 
